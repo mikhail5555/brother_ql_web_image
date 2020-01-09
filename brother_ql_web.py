30d8344afb3636dad0969402f55bc89a4e1d631d
AllergicDuck <mikhail.epifanov@gmail.com>
2020-01-09 18:10:08 +0100

    Added image support

--- brother_ql_web.py
+++ brother_ql_web.py
@@ -4,22 +4,21 @@
 This is a web service to print labels on Brother QL label printers.
 """
 
-import sys, logging, random, json, argparse
+import sys, logging, random, json, argparse, os
 from io import BytesIO
 
 from bottle import run, route, get, post, response, request, jinja2_view as view, static_file, redirect
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
 from brother_ql.devicedependent import models, label_type_specs, label_sizes
 from brother_ql.devicedependent import ENDLESS_LABEL, DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL
 from brother_ql import BrotherQLRaster, create_label
 from brother_ql.backends import backend_factory, guess_backend
 
-from font_helpers import get_fonts
-
 logger = logging.getLogger(__name__)
 
-LABEL_SIZES = [ (name, label_type_specs[name]['name']) for name in label_sizes]
+LABEL_SIZES = [(name, label_type_specs[name]['name'], label_type_specs[name]['dots_printable']) for name in label_sizes]
+IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.bmp']
 
 try:
     with open('config.json') as fh:
@@ -33,120 +32,76 @@ except FileNotFoundError as e:
 def index():
     redirect('/labeldesigner')
 
+
 @route('/static/<filename:path>')
 def serve_static(filename):
     return static_file(filename, root='./static')
 
+
 @route('/labeldesigner')
 @view('labeldesigner.jinja2')
 def labeldesigner():
-    font_family_names = sorted(list(FONTS.keys()))
-    return {'font_family_names': font_family_names,
-            'fonts': FONTS,
-            'label_sizes': LABEL_SIZES,
+    return {'label_sizes': LABEL_SIZES,
+            'files': FILE_NAMES,
             'website': CONFIG['WEBSITE'],
             'label': CONFIG['LABEL']}
+
+
+@route('/upload', method='POST')
+def do_upload():
+    upload = request.files.get('upload')
+    name, ext = os.path.splitext(upload.filename)
+
+    if ext not in IMAGE_EXTENSIONS:
+        return "File extension not allowed."
+
+    file_path = os.path.join(IMG_DIR, upload.filename)
+
+    upload.save(file_path)
+    update_files()
+
+    redirect('/labeldesigner')
+
 
 def get_label_context(request):
     """ might raise LookupError() """
 
-    d = request.params.decode() # UTF-8 decoded form data
+    d = request.params.decode()  # UTF-8 decoded form data
 
-    font_family = d.get('font_family').rpartition('(')[0].strip()
-    font_style  = d.get('font_family').rpartition('(')[2].rstrip(')')
     context = {
-      'text':          d.get('text', None),
-      'font_size': int(d.get('font_size', 100)),
-      'font_family':   font_family,
-      'font_style':    font_style,
-      'label_size':    d.get('label_size', "62"),
-      'kind':          label_type_specs[d.get('label_size', "62")]['kind'],
-      'margin':    int(d.get('margin', 10)),
-      'threshold': int(d.get('threshold', 70)),
-      'align':         d.get('align', 'center'),
-      'orientation':   d.get('orientation', 'standard'),
-      'margin_top':    float(d.get('margin_top',    24))/100.,
-      'margin_bottom': float(d.get('margin_bottom', 45))/100.,
-      'margin_left':   float(d.get('margin_left',   35))/100.,
-      'margin_right':  float(d.get('margin_right',  35))/100.,
+        'file_name': d.get('file_name', "image.png"),
+        'label_size': d.get('label_size', "62"),
+        'kind': label_type_specs[d.get('label_size', "62")]['kind'],
+        'orientation': d.get('orientation', 'standard'),
     }
-    context['margin_top']    = int(context['font_size']*context['margin_top'])
-    context['margin_bottom'] = int(context['font_size']*context['margin_bottom'])
-    context['margin_left']   = int(context['font_size']*context['margin_left'])
-    context['margin_right']  = int(context['font_size']*context['margin_right'])
-
-    def get_font_path(font_family_name, font_style_name):
-        try:
-            if font_family_name is None or font_style_name is None:
-                font_family_name = CONFIG['LABEL']['DEFAULT_FONTS']['family']
-                font_style_name =  CONFIG['LABEL']['DEFAULT_FONTS']['style']
-            font_path = FONTS[font_family_name][font_style_name]
-        except KeyError:
-            raise LookupError("Couln't find the font & style")
-        return font_path
-
-    context['font_path'] = get_font_path(context['font_family'], context['font_style'])
-
-    def get_label_dimensions(label_size):
-        try:
-            ls = label_type_specs[context['label_size']]
-        except KeyError:
-            raise LookupError("Unknown label_size")
-        return ls['dots_printable']
-
-    width, height = get_label_dimensions(context['label_size'])
-    if height > width: width, height = height, width
-    if context['orientation'] == 'rotated': height, width = width, height
-    context['width'], context['height'] = width, height
 
     return context
 
-def create_label_im(text, **kwargs):
-    label_type = kwargs['kind']
-    im_font = ImageFont.truetype(kwargs['font_path'], kwargs['font_size'])
-    im = Image.new('L', (20, 20), 'white')
-    draw = ImageDraw.Draw(im)
-    # workaround for a bug in multiline_textsize()
-    # when there are empty lines in the text:
-    lines = []
-    for line in text.split('\n'):
-        if line == '': line = ' '
-        lines.append(line)
-    text = '\n'.join(lines)
-    linesize = im_font.getsize(text)
-    textsize = draw.multiline_textsize(text, font=im_font)
-    width, height = kwargs['width'], kwargs['height']
-    if kwargs['orientation'] == 'standard':
-        if label_type in (ENDLESS_LABEL,):
-            height = textsize[1] + kwargs['margin_top'] + kwargs['margin_bottom']
-    elif kwargs['orientation'] == 'rotated':
-        if label_type in (ENDLESS_LABEL,):
-            width = textsize[0] + kwargs['margin_left'] + kwargs['margin_right']
-    im = Image.new('L', (width, height), 'white')
-    draw = ImageDraw.Draw(im)
-    if kwargs['orientation'] == 'standard':
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            vertical_offset  = (height - textsize[1])//2
-            vertical_offset += (kwargs['margin_top'] - kwargs['margin_bottom'])//2
-        else:
-            vertical_offset = kwargs['margin_top']
-        horizontal_offset = max((width - textsize[0])//2, 0)
-    elif kwargs['orientation'] == 'rotated':
-        vertical_offset  = (height - textsize[1])//2
-        vertical_offset += (kwargs['margin_top'] - kwargs['margin_bottom'])//2
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            horizontal_offset = max((width - textsize[0])//2, 0)
-        else:
-            horizontal_offset = kwargs['margin_left']
-    offset = horizontal_offset, vertical_offset
-    draw.multiline_text(offset, text, (0), font=im_font, align=kwargs['align'])
-    return im
 
-@get('/api/preview/text')
-@post('/api/preview/text')
+def image_exists(image_name):
+    update_files()
+    return image_name in FILE_NAMES
+
+
+@get('/api/preview/image')
+@post('/api/preview/image')
 def get_preview_image():
-    context = get_label_context(request)
-    im = create_label_im(**context)
+    try:
+        context = get_label_context(request)
+    except LookupError as e:
+        return e.msg
+
+    if not image_exists(context["file_name"]):
+        logger.warning('Exception happened: Image not in directory')
+        return "Image not in directory"
+
+    im = Image.open(os.path.join(IMG_DIR, context["file_name"]))
+
+    if context['orientation'] == 'rotated+90':
+        im = im.transpose(Image.ROTATE_90)
+    elif context['orientation'] == 'rotated-90':
+        im = im.transpose(Image.ROTATE_270)
+
     return_format = request.query.get('return_format', 'png')
     if return_format == 'base64':
         import base64
@@ -156,15 +111,42 @@ def get_preview_image():
         response.set_header('Content-type', 'image/png')
         return image_to_png_bytes(im)
 
+
+@get('/api/delete/image')
+@post('/api/delete/image')
+def delete_image():
+    try:
+        context = get_label_context(request)
+    except LookupError as e:
+        return e.msg
+
+    if not image_exists(context["file_name"]):
+        logger.warning('Exception happened: Image not in directory')
+        return "Image not in directory"
+
+    image = IMG_DIR + context["file_name"]
+
+    os.remove(image)
+    update_files()
+
+    redirect('/labeldesigner')
+
+
+def update_files():
+    global FILE_NAMES
+    FILE_NAMES = [f for f in os.listdir(IMG_DIR) if (os.path.isfile(os.path.join(IMG_DIR, f)) and os.path.splitext(f)[1] in IMAGE_EXTENSIONS)]
+
+
 def image_to_png_bytes(im):
     image_buffer = BytesIO()
     im.save(image_buffer, format="PNG")
     image_buffer.seek(0)
     return image_buffer.read()
 
-@post('/api/print/text')
-@get('/api/print/text')
-def print_text():
+
+@post('/api/print/image')
+@get('/api/print/image')
+def print_image():
     """
     API to print a label
 
@@ -182,12 +164,12 @@ def print_text():
         return_dict['error'] = e.msg
         return return_dict
 
-    if context['text'] is None:
-        return_dict['error'] = 'Please provide the text for the label'
+    if not image_exists(context["file_name"]):
+        return_dict['message'] = "Image not in directory"
+        logger.warning('Exception happened: Image not in directory')
         return return_dict
 
-    im = create_label_im(**context)
-    if DEBUG: im.save('sample-out.png')
+    im = Image.open(os.path.join(IMG_DIR, context["file_name"]))
 
     if context['kind'] == ENDLESS_LABEL:
         rotate = 0 if context['orientation'] == 'standard' else 90
@@ -195,7 +177,7 @@ def print_text():
         rotate = 'auto'
 
     qlr = BrotherQLRaster(CONFIG['PRINTER']['MODEL'])
-    create_label(qlr, im, context['label_size'], threshold=context['threshold'], cut=True, rotate=rotate)
+    create_label(qlr, im, context['label_size'], cut=True, rotate=rotate)
 
     if not DEBUG:
         try:
@@ -212,17 +194,24 @@ def print_text():
     if DEBUG: return_dict['data'] = str(qlr.data)
     return return_dict
 
+
 def main():
-    global DEBUG, FONTS, BACKEND_CLASS, CONFIG
+    global DEBUG, BACKEND_CLASS, CONFIG, IMG_DIR, FILE_NAMES
+
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--port', default=False)
     parser.add_argument('--loglevel', type=lambda x: getattr(logging, x.upper()), default=False)
-    parser.add_argument('--font-folder', default=False, help='folder for additional .ttf/.otf fonts')
-    parser.add_argument('--default-label-size', default=False, help='Label size inserted in your printer. Defaults to 62.')
-    parser.add_argument('--default-orientation', default=False, choices=('standard', 'rotated'), help='Label orientation, defaults to "standard". To turn your text by 90°, state "rotated".')
+    parser.add_argument('--default-label-size', default=False,
+                        help='Label size inserted in your printer. Defaults to 62.')
+    parser.add_argument('--default-orientation', default=False, choices=('standard', 'rotated'),
+                        help='Label orientation, defaults to "standard". To turn your text by 90°, state "rotated".')
     parser.add_argument('--model', default=False, choices=models, help='The model of your printer (default: QL-500)')
-    parser.add_argument('printer',  nargs='?', default=False, help='String descriptor for the printer to use (like tcp://192.168.0.23:9100 or file:///dev/usb/lp0)')
+    parser.add_argument('printer', nargs='?', default=False,
+                        help='String descriptor for the printer to use (like tcp://192.168.0.23:9100 or file:///dev/usb/lp0)')
     args = parser.parse_args()
+
+    IMG_DIR = "./img/"
+    update_files()
 
     if args.printer:
         CONFIG['PRINTER']['PRINTER'] = args.printer
@@ -251,12 +240,6 @@ def main():
     if args.default_orientation:
         CONFIG['LABEL']['DEFAULT_ORIENTATION'] = args.default_orientation
 
-    if args.font_folder:
-        ADDITIONAL_FONT_FOLDER = args.font_folder
-    else:
-        ADDITIONAL_FONT_FOLDER = CONFIG['SERVER']['ADDITIONAL_FONT_FOLDER']
-
-
     logging.basicConfig(level=LOGLEVEL)
 
     try:
@@ -268,29 +251,8 @@ def main():
     if CONFIG['LABEL']['DEFAULT_SIZE'] not in label_sizes:
         parser.error("Invalid --default-label-size. Please choose on of the following:\n:" + " ".join(label_sizes))
 
-    FONTS = get_fonts()
-    if ADDITIONAL_FONT_FOLDER:
-        FONTS.update(get_fonts(ADDITIONAL_FONT_FOLDER))
-
-    if not FONTS:
-        sys.stderr.write("Not a single font was found on your system. Please install some or use the \"--font-folder\" argument.\n")
-        sys.exit(2)
-
-    for font in CONFIG['LABEL']['DEFAULT_FONTS']:
-        try:
-            FONTS[font['family']][font['style']]
-            CONFIG['LABEL']['DEFAULT_FONTS'] = font
-            logger.debug("Selected the following default font: {}".format(font))
-            break
-        except: pass
-    if CONFIG['LABEL']['DEFAULT_FONTS'] is None:
-        sys.stderr.write('Could not find any of the default fonts. Choosing a random one.\n')
-        family =  random.choice(list(FONTS.keys()))
-        style =   random.choice(list(FONTS[family].keys()))
-        CONFIG['LABEL']['DEFAULT_FONTS'] = {'family': family, 'style': style}
-        sys.stderr.write('The default font is now set to: {family} ({style})\n'.format(**CONFIG['LABEL']['DEFAULT_FONTS']))
-
     run(host=CONFIG['SERVER']['HOST'], port=PORT, debug=DEBUG)
+
 
 if __name__ == "__main__":
     main()
